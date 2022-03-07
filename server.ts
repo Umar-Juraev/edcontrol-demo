58429b50020dd0688a121a19fd0bb85e0cf88165
Umar-Juraev <kolibri1108j@gmail.com>
2022-03-07 13:42:52 +0500

    fixed

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
-var jsonServer = require('json-server');
-var server = jsonServer.create();
-var router = jsonServer.router('./db.json');
-var middlewares = jsonServer.defaults({
+const jsonServer = require('json-server')
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
+
+const middlewares = jsonServer.defaults({
   static: './build'
 });
-var PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
   '/api/*': '/$1',
